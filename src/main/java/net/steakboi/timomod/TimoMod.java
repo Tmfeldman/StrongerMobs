5a210b82cf809ab724db26ced5986e48b5a1dc82
Timo <feldman.timo@gmail.com>
2025-11-21 13:45:22 -0600

    copied 1.21.10 tutorial

--- src/main/java/net/steakboi/timomod/TimoMod.java
+++ src/main/java/net/steakboi/timomod/TimoMod.java
@@ -1,16 +1,16 @@
-package net.steakboi.templateMod;
+package net.steakboi.timomod;
 
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class TemplateMod implements ModInitializer {
-	public static final String MOD_ID = "template-mod";
+public class TimoMod implements ModInitializer {
+	public static final String MOD_ID = "timomod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+
 	}
 }
---
+++ src/main/java/net/steakboi/timomod/TimoModClient.java
@@ -0,0 +1,10 @@
+package net.steakboi.timomod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TimoModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
